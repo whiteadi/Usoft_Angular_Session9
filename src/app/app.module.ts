2942877394723ef59dc247f2e5f6860a9bd91883
Adrian Albu <adrian.albu@usoft.com>
2021-05-18 10:34:52 +0200

    session 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule} from '@angular/forms';
 import { AgGridModule } from 'ag-grid-angular';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,10 @@ import { PersonsComponent } from './persons/persons.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NewPersonComponent } from './new-person/new-person.component';
 import { PersonsGridComponent } from './persons-grid/persons-grid.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -27,6 +32,11 @@ import { PersonsGridComponent } from './persons-grid/persons-grid.component';
     FormsModule,
     ReactiveFormsModule,
     AgGridModule.withComponents([]),
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
